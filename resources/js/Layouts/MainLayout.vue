967d740ae28972d522acd1dbd5efc2983d8490e4
osmar <osmarrod18@gmail.com>
2024-06-18 23:15:44 +0100

    misc files and added debugbar

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="min-h-screen dark:bg-gray-900">
+      <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full h-full">
+        <div class="container mx-auto">
+          <nav class="p-4 flex items-center justify-between">
+            <div class="text-lg font-medium text-white">
+              <Link :href="route('listing.index')">Listings</Link>
+            </div>
+            <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center ">
+              <Link :href="route('listing.index')">Property Realtor App</Link>
+            </div>
+            <div v-if="user" class="flex items-center gap-4">
+              <Link
+                class="text-gray-500 relative pr-2 py-2 text-lg"
+                :href="route('notification.index')"
+              >
+                🔔
+                <div v-if="notificationCount" class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center">
+                  {{ notificationCount }}
+                </div>
+              </Link>
+
+              <Link class="text-sm text-gray-300 " :href="route('realtor.listing.index')">{{ user.name }}</Link>
+              <Link :href="route('realtor.listing.create')" class="btn-primary">+ New Listing</Link>
+              <div>
+                <Link class="text-sm text-gray-300"  :href="route('logout')" method="delete" as="button">Logout</Link>
+              </div>
+            </div>
+            <div v-else class="flex items-center gap-2 text-gray-300">
+              <Link :href="route('user-account.create')">Register</Link>
+              <Link :href="route('login')">Sign-In</Link>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      <main class="container mx-auto p-4 w-full">
+        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+          {{ flashSuccess }}
+        </div>
+        <slot>Default</slot>
+      </main>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.flash.success,
+)
+const user = computed(
+  () => page.props.user,
+)
+const notificationCount = computed(
+  () => Math.min(page.props.user.notificationCount, 9),
+)
+</script>
