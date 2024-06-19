d6aab43042ca0065759afad1d21620b79dbcea3e
osmar <osmarrod18@gmail.com>
2024-06-19 23:37:25 +0100

    added porperty listing realted CRUD

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Response;
+
+class ListingController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Listing::class, 'listing');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $filters = $request->only([
+            'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
+        ]);
+
+        return inertia(
+            'Listing/Index',
+            [
+                'filters' => $filters,
+                'listings' => Listing::mostRecent()
+                    ->filter($filters)
+                    ->withoutSold()
+                    ->paginate(10)
+                    ->withQueryString()
+            ]
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Listing $listing)
+    {
+        // if (Auth::user()->cannot('view', $listing)) {
+        //     abort(403);
+        // }
+        // $this->authorize('view', $listing);
+        $listing->load(['images']);
+        $offer = !Auth::user() ?
+            null : $listing->offers()->byMe()->first();
+
+        return inertia(
+            'Listing/Show',
+            [
+                'listing' => $listing,
+                'offerMade' => $offer
+            ]
+        );
+    }
+}
---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,12 @@
+<template>
+  <span>
+    {{ listing.street }} {{ listing.street_nr }}, {{
+      listing.city }}
+  </span>
+</template>
+
+<script setup>
+defineProps({
+  listing: Object,
+})
+</script>
