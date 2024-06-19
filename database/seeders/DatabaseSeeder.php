db993deea67454ce374ab41467b091fe04a0600b
osmar <osmarrod18@gmail.com>
2024-06-19 23:22:59 +0100

    added factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,37 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+//        \App\Models\User::factory()->create([
+//            'name' => 'Admin',
+//            'email' => 'realtor@example.com',
+//            'password' => Hash::make('Admin@123'),
+//            'is_admin' => true
+//        ]);
+//        \App\Models\User::factory()->create([
+//            'name' => 'User',
+//            'email' => 'user@example.com',
+//            'password' => Hash::make('Admin@123'),
+//        ]);
+        \App\Models\Listing::factory(10)->create([
+            'by_user_id' => 1,
+        ]);
+        \App\Models\Listing::factory(10)->create([
+            'by_user_id' => 2
         ]);
     }
 }
