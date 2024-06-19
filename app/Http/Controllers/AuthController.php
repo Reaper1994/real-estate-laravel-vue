8654bbf686702c5eb8210a1ab951d552eae34faf
osmar <osmarrod18@gmail.com>
2024-06-19 23:41:52 +0100

    refactored auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Inertia\Response;
+use Inertia\ResponseFactory;
+
+class AuthController extends Controller
+{
+    /**
+     * @return Response|ResponseFactory
+     */
+    public function create()
+    {
+        return inertia('Auth/Login');
+    }
+
+    public function store(Request $request)
+    {
+        if (!Auth::attempt($request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]), true)) {
+            throw ValidationException::withMessages([
+                'email' => 'Authentication failed'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended('/listing');
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('listing.index');
+    }
+}
