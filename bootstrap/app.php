97f614071a774a5bb0009066e0c6d226c7f404a1
osmar <osmarrod18@gmail.com>
2024-06-19 23:21:39 +0100

    added verified signed and throttle middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->alias([
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        ]);
 
         //
     })
