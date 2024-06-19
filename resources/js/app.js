7fb7ca6e12dcc83a7a92fac1dee75b8d3b4779de
osmar <osmarrod18@gmail.com>
2024-06-19 23:15:49 +0100

    updated main layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,20 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import MainLayout from '@/Layouts/MainLayout.vue'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue')
+
+        const page = await pages[`./Pages/${name}.vue`]()
+        page.default.layout = page.default.layout || MainLayout
+
+        return page
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
