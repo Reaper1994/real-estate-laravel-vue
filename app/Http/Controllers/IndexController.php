c1d18f2e365a525a1e69418ec0a8c2165c9d3f0a
osmar <osmarrod18@gmail.com>
2024-06-19 23:33:12 +0100

    added home page components

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return inertia(
+            'Index/Index',
+            [
+                'message' => 'Hello from Laravel!'
+            ]
+        );
+    }
+
+    public function show()
+    {
+        return inertia('Index/Show');
+    }
+}
