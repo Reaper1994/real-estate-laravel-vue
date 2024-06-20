423a33f8cbfd23354bd7d619a89f188142feed6b
osmar <osmarrod18@gmail.com>
2024-06-20 16:55:27 +0100

    minor refactor

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -45,12 +45,6 @@ class Listing extends Model
 
     public function scopeWithoutSold(Builder $query): Builder
     {
-        // return $query->doesntHave('offers')
-        //     ->orWhereHas(
-        //         'offers',
-        //         fn (Builder $query) => $query->whereNull('accepted_at')
-        //             ->whereNull('rejected_at')
-        //     );
         return $query->whereNull('sold_at');
     }
 
