b6bb328bf2d8bfae0f336c27c67fc06927fb0eaf
osmar <osmarrod18@gmail.com>
2024-06-27 18:17:00 +0100

    imporvements

--- database/factories/ListingFactory.php
+++ database/factories/ListingFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
  */
-class ListingFactory extends Factory
+final class ListingFactory extends Factory
 {
     /**
      * Define the model's default state.
